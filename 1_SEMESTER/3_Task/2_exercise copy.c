a25aaa3d0f5b0398c53cba770a8a2bc82f53c2a8
Leillo <o.leobernal@gmail.com>
2026-09-21 23:44:24 -0600

    I added just one more exercise

--- 1_SEMESTER/3_Task/2_exercise copy.c
+++ 1_SEMESTER/3_Task/2_exercise copy.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
-        
+    int num = 0;
+    int res = 0;
+    char u = 163;
+    while(num <= 42){
+        printf("Introduce un n%cmero: \n", u);
+        scanf("%d", &num);
+        res += num;
+    }
+
+    printf("El resultado de la sumatoria de todos los n%cmeros que introdujiste fue de %d", u, res);
 }
